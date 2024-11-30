f346103e794aba94e4bd1b301b0fccca20707f83
MatthewMa929 <MatthewMa929@Gmail.com>
2024-11-30 13:26:53 -0500

    PlayerList.php

--- playerList.php
+++ playerList.php
@@ -1,7 +1,18 @@
 <?php
-include "connect.php";
+    include "connect.php";
 
-// Fetch players from the database
-$sql = "SELECT * FROM players ORDER BY total_points DESC";
-$result = mysqli_query($conn, $sql);
+    $sql = "SELECT name, sport, position, real_team, total_points, status FROM players";
+    $result = mysqli_query($conn, $sql);
+
+    $stmt = $conn->prepare($sql); $stmt->execute(); 
+    echo 'Player information: <br />'; 
+    while ($row = $stmt->fetch()) { 
+        echo '----------------------------------------<br />';
+        echo "Name: " . $row["full_name"] . "<br />";
+        echo "Sport: " . $row["sport"] . "<br />";
+        echo "Position: " . $row["position"] . "<br />";
+        echo "Real Team: " . $row["real_team"] . "<br />";
+        echo "Total Points: " . $row["fantasy_points"] . "<br />";
+        echo "Status: " . $row["availability_status"] . "<br />";
+    }
 ?>
